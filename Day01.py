c588d58226f9078bd232c5ff550409cf5d32f1b6
manei11 <morad.delavar@gmail.com>
2026-08-14 11:23:37 +0330

    Create day01.py

---
+++ Day01.py
@@ -0,0 +1,15 @@
+for x in range(5):
+    for y in range(5):
+        Pydle(x, y, "", "white")
+
+if x in [1, 1] and y == 1:
+    Pydle(x, y, "", "blue")
+
+if x in [2, 2] and y == 1:
+    Pydle(x, y, "", "black")
+
+if x in [2, 3] and y in [0, 4]:
+    Pydle(x, y, "", "blue")
+
+if x in [3, 4] and y in [1, 2, 3]:
+    Pydle(x, y, "", "blue")
